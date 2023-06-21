8393d3dcf46c2476898e54e6b12a8156c0a41687
Richard Neumann <r.neumann@homeinfo.de>
2023-06-21 18:03:20 +0200

    Implement types.xml merger

---
+++ src/lib.rs
@@ -0,0 +1,132 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
+use std::ops::Add;
+use std::str::FromStr;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Types {
+    #[serde(rename = "type")]
+    types: Vec<Type>,
+}
+
+impl Add for Types {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut map = HashMap::from(&self);
+        map.extend(HashMap::from(&rhs));
+        let mut types: Vec<Type> = map.into_values().cloned().collect();
+        types.sort_by(|lhs, rhs| lhs.name.cmp(&rhs.name));
+        Self { types }
+    }
+}
+
+impl Display for Types {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "<types>{}</types>",
+            self.types
+                .iter()
+                .map(|typ| typ.to_string())
+                .collect::<String>()
+        )
+    }
+}
+
+impl<'a> From<&'a Types> for HashMap<&'a str, &'a Type> {
+    fn from(types: &'a Types) -> Self {
+        types
+            .types
+            .iter()
+            .map(|typ| (typ.name.as_str(), typ))
+            .collect()
+    }
+}
+
+impl FromStr for Types {
+    type Err = serde_xml_rs::Error;
+
+    fn from_str(xml: &str) -> Result<Self, Self::Err> {
+        serde_xml_rs::from_str(xml)
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Type {
+    name: String,
+    nominal: u8,
+    lifetime: u32,
+    restock: u32,
+    min: u8,
+    quantmin: i64,
+    quantmax: i64,
+    cost: u32,
+    flags: Flags,
+    category: Option<Named>,
+    usage: Option<Vec<Named>>,
+    value: Option<Vec<Named>>,
+}
+
+impl Display for Type {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, r#"<type name="{}">"#, self.name)?;
+        write!(f, "<nominal>{}</nominal>", self.nominal)?;
+        write!(f, "<lifetime>{}</lifetime>", self.lifetime)?;
+        write!(f, "<restock>{}</restock>", self.restock)?;
+        write!(f, "<min>{}</min>", self.min)?;
+        write!(f, "<quantmin>{}</quantmin>", self.quantmin)?;
+        write!(f, "<quantmax>{}</quantmax>", self.quantmax)?;
+        write!(f, "{}", self.flags)?;
+
+        if let Some(category) = &self.category {
+            write!(f, r#"<category name="{}"/>"#, category.name)?;
+        }
+
+        if let Some(usages) = &self.usage {
+            for usage in usages {
+                write!(f, r#"<usage name="{}"/>"#, usage.name)?;
+            }
+        }
+
+        if let Some(values) = &self.value {
+            for value in values {
+                write!(f, r#"<value name="{}"/>"#, value.name)?;
+            }
+        }
+
+        write!(f, "</type>")
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Flags {
+    count_in_cargo: u64,
+    count_in_hoarder: u64,
+    count_in_map: u64,
+    count_in_player: u64,
+    crafted: u64,
+    deloot: u64,
+}
+
+impl Display for Flags {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            r#"<flags count_in_cargo="{}" count_in_hoarder="{}" count_in_map="{}" count_in_player="{}" crafted="{}" deloot="{}"/>"#,
+            self.count_in_cargo,
+            self.count_in_hoarder,
+            self.count_in_map,
+            self.count_in_player,
+            self.crafted,
+            self.deloot
+        )
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Named {
+    #[serde(rename = "name")]
+    name: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use clap::Parser;
+use std::fs::{read_to_string, write};
+use std::process::exit;
+use std::str::FromStr;
+use typesxml::Types;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(index = 1)]
+    base: String,
+
+    #[arg(index = 2)]
+    extension: String,
+
+    #[arg(long, short)]
+    output: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let merger = Types::from_str(&read_to_string(args.base).unwrap_or_else(|error| {
+        eprintln!("{}", error);
+        exit(1);
+    }))
+    .unwrap_or_else(|error| {
+        eprintln!("{}", error);
+        exit(2);
+    }) + Types::from_str(&read_to_string(args.extension).unwrap_or_else(|error| {
+        eprintln!("{}", error);
+        exit(1);
+    }))
+    .unwrap_or_else(|error| {
+        eprintln!("{}", error);
+        exit(2);
+    });
+
+    if let Some(file) = args.output {
+        write(file, merger.to_string()).unwrap_or_else(|error| {
+            eprintln!("{}", error);
+            exit(3);
+        })
+    } else {
+        println!("{}", merger);
+    }
 }
