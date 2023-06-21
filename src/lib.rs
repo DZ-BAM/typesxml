58ec38320ee6fbe8b14e5cdf630579fc7393d086
Richard Neumann <mail@richard-neumann.de>
2023-06-21 22:07:31 +0200

    Move to quick-xml

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use serde::ser::Error;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
@@ -26,11 +27,8 @@ impl Display for Types {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "<types>{}</types>",
-            self.types
-                .iter()
-                .map(|typ| typ.to_string())
-                .collect::<String>()
+            "{}",
+            quick_xml::se::to_string(self).map_err(std::fmt::Error::custom)?
         )
     }
 }
@@ -46,15 +44,16 @@ impl<'a> From<&'a Types> for HashMap<&'a str, &'a Type> {
 }
 
 impl FromStr for Types {
-    type Err = serde_xml_rs::Error;
+    type Err = quick_xml::de::DeError;
 
     fn from_str(xml: &str) -> Result<Self, Self::Err> {
-        serde_xml_rs::from_str(xml)
+        quick_xml::de::from_str(xml)
     }
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Type {
+    #[serde(rename = "@name")]
     name: String,
     nominal: u8,
     lifetime: u32,
@@ -69,65 +68,24 @@ pub struct Type {
     value: Option<Vec<Named>>,
 }
 
-impl Display for Type {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, r#"<type name="{}">"#, self.name)?;
-        write!(f, "<nominal>{}</nominal>", self.nominal)?;
-        write!(f, "<lifetime>{}</lifetime>", self.lifetime)?;
-        write!(f, "<restock>{}</restock>", self.restock)?;
-        write!(f, "<min>{}</min>", self.min)?;
-        write!(f, "<quantmin>{}</quantmin>", self.quantmin)?;
-        write!(f, "<quantmax>{}</quantmax>", self.quantmax)?;
-        write!(f, "<cost>{}</cost>", self.cost)?;
-        write!(f, "{}", self.flags)?;
-
-        if let Some(category) = &self.category {
-            write!(f, r#"<category name="{}"/>"#, category.name)?;
-        }
-
-        if let Some(usages) = &self.usage {
-            for usage in usages {
-                write!(f, r#"<usage name="{}"/>"#, usage.name)?;
-            }
-        }
-
-        if let Some(values) = &self.value {
-            for value in values {
-                write!(f, r#"<value name="{}"/>"#, value.name)?;
-            }
-        }
-
-        write!(f, "</type>")
-    }
-}
-
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Flags {
+    #[serde(rename = "@count_in_cargo")]
     count_in_cargo: u64,
+    #[serde(rename = "@count_in_hoarder")]
     count_in_hoarder: u64,
+    #[serde(rename = "@count_in_map")]
     count_in_map: u64,
+    #[serde(rename = "@count_in_player")]
     count_in_player: u64,
+    #[serde(rename = "@crafted")]
     crafted: u64,
+    #[serde(rename = "@deloot")]
     deloot: u64,
-}
-
-impl Display for Flags {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            r#"<flags count_in_cargo="{}" count_in_hoarder="{}" count_in_map="{}" count_in_player="{}" crafted="{}" deloot="{}"/>"#,
-            self.count_in_cargo,
-            self.count_in_hoarder,
-            self.count_in_map,
-            self.count_in_player,
-            self.crafted,
-            self.deloot
-        )
-    }
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Named {
-    #[serde(rename = "name")]
+    #[serde(rename = "@name")]
     name: String,
 }
