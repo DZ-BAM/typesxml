0352593cfe129f9f737cfca8363ebd27a6fc48ce
Richard Neumann <mail@richard-neumann.de>
2023-06-22 17:32:50 +0200

    Use absolute namespace qualifiers

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@ mod r#type;
 mod types;
 mod value;
 
-use std::fmt::{Display, Formatter};
-
 pub use flags::Flags;
 pub use named::Named;
 pub use r#type::Type;
@@ -14,9 +12,9 @@ pub use tier::Tier;
 pub use types::Types;
 pub use value::Value;
 
-pub(crate) fn fmt_vec<T>(f: &mut Formatter<'_>, names: &[T]) -> std::fmt::Result
+pub(crate) fn fmt_vec<T>(f: &mut std::fmt::Formatter<'_>, names: &[T]) -> std::fmt::Result
 where
-    T: Display,
+    T: std::fmt::Display,
 {
     write!(f, "usages:\t[ ")?;
 
